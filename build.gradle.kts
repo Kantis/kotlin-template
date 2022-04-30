1441dccbe3792a72ca0cc3d8eaf90522090d428d
Emil Kantis <emil.kantis@protonmail.com>
2022-04-30 23:11:59 +0200

    Update kotlin + kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val targetJdk = JavaVersion.VERSION_17
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.21"
 }
 
 tasks.named<Wrapper>("wrapper") {
