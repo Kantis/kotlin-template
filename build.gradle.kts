649ffd235c3b0ea6a780352409e53df8a2ccdb5d
Emil Kantis <emil.kantis@protonmail.com>
2024-06-20 14:36:20 +0200

    Using convention plugin for build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,8 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "2.0.0"
-}
-
-repositories {
-    mavenCentral()
+   id("kotlin-conventions")
 }
 
 dependencies {
-    testImplementation(libs.kotest.runner.junit5)
-    testImplementation(libs.kotest.property)
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_21)
-    }
-}
-
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
+   testImplementation(libs.kotest.runner.junit5)
+   testImplementation(libs.kotest.property)
 }
