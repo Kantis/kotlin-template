7cbbcd1735a0b4b1240a9070c70e80f5dde33fcd
Emil Kantis <emil.kantis@protonmail.com>
2024-06-20 16:03:30 +0200

    Formatted test file

--- src/test/kotlin/ExampleTest.kt
+++ src/test/kotlin/ExampleTest.kt
@@ -1,10 +1,8 @@
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class ExampleTest : StringSpec(
-    {
-        "hello" {
-            1 shouldBe 1
-        }
-    }
-)
+class ExampleTest : StringSpec({
+   "hello" {
+      1 shouldBe 1
+   }
+})
